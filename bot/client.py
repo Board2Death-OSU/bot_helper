3fa73c8a097df57812fc4137fd1f1f2371c01724
jklypchak13 <jarod.klypchak@gmail.com>
2019-08-29 15:12:26 -0400

    Added initial client for testing

---
+++ bot/client.py
@@ -0,0 +1,62 @@
+
+import discord
+
+
+class Client(discord.Client):
+
+    def __init__(self):
+        self.on_message_functions = []
+        self.on_message_args = []
+        super()
+
+    def register_on_message_callback(self, fun, args):
+        """
+        This function adds a call back function to be executed when the
+        client receives a message.
+
+        The function expects a function as the first argument, and an array of arguments,
+        that will be unpacked for the function call.
+
+        When called, the function will receive the discord message, then the arguments.
+        The function should return a tuple containing the message to send, and then the channel
+        to send the response to.s
+        """
+        self.on_message_functions.append(fun)
+        self.on_message_args.append(args)
+
+        print('Successfully Registered {}'.format(fun.__name__))
+
+    async def on_ready(self):
+        print('Successfully Logged in as {0}', self.user)
+        pass
+
+    async def on_message(self, message):
+        """
+        Called when a message is received, executed all registered callback functions,
+        passing in the message and there arguments.
+        """
+
+        # Don't Respond to Yourself
+        if message.author == self.user:
+            return
+
+        # Responses to be sent after processing all messages.
+        responses = []
+
+        content = str(message.content)
+        for fun, args in zip(self.on_message_functions, self.on_message_args):
+            print('Exectuing {0} with these args: {1}'.format(
+                fun.__name__, args))
+            value = fun(message, *args)
+            if value is not None:
+                responses.append(value)
+        for (response, channel) in responses:
+            await channel.send(response)
+
+
+def temp(a):
+    print('x')
+
+
+temp = Client()
+temp.register_on_message_callback(temp, [1])
