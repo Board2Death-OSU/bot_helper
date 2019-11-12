e0a0526c84d17768456a42bf2f956c20a8277f2e
jklypchak13 <jarod.klypchak@gmail.com>
2019-11-12 10:51:26 -0500

    Added ImageHandler, with image responses.

--- bot/client.py
+++ bot/client.py
@@ -7,6 +7,9 @@ class Client(discord.Client):
     def __init__(self):
         self.on_message_functions = []
         self.on_message_args = []
+
+        self.on_message_file_functions = []
+        self.on_message_file_args = []
         super().__init__()
 
     def register_on_message_callback(
@@ -25,6 +28,14 @@ class Client(discord.Client):
         self.on_message_functions.append(fun)
         self.on_message_args.append(args)
 
+    def register_on_message_send_file_callback(
+        self,
+        fun: Callable[[discord.Message, any], Tuple[str, discord.TextChannel]],
+        args: List[any]
+    ) -> None:
+        self.on_message_file_functions.append(fun)
+        self.on_message_file_args.append(args)
+
     async def on_ready(self) -> None:
         print('Successfully Logged in as {0}'.format(self.user))
 
@@ -40,6 +51,7 @@ class Client(discord.Client):
 
         # Responses to be sent after processing all messages.
         responses: List[Tuple[str, discord.TextChannel]] = []
+        file_responses = []
 
         content: str = str(message.content)
         for fun, args in zip(self.on_message_functions, self.on_message_args):
@@ -47,6 +59,15 @@ class Client(discord.Client):
             if value is not None:
                 responses.append(value)
 
+        for fun, args in zip(self.on_message_file_functions, self.on_message_file_args):
+            value = fun(message, *args)
+            if value is not None:
+                file_responses.append(value)
+
         for (response, channel) in responses:
             if response is not None and response != '':
                 await channel.send(response)
+
+        for (response, channel) in file_responses:
+            if response is not None and response != '':
+                await channel.send('', file=discord.File(response))
---
+++ resources/image_handler.py
@@ -0,0 +1,30 @@
+import random
+from typing import List
+import os
+
+
+class ImageHandler:
+
+    def __init__(self, images: List[str], keyword: str, channel):
+        self.images = images
+        self.keyword = keyword
+        self.channel = channel
+
+    def get_call_back(self):
+        def _call_back(message):
+            channel = message.channel
+            message = message.content
+            if str(channel) == self.channel:
+                if self.keyword in message:
+                    index = random.randint(0, len(self.images) - 1)
+                    file_name = self.images[index]
+                    return file_name, channel
+        return _call_back
+
+    @staticmethod
+    def get_files(directory):
+        temp = os.listdir(directory)
+        result = []
+        for img in temp:
+            result.append(directory + '/' + img)
+        return result
